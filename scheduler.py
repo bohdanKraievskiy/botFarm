9883fa77251ab226204a438eb94dfd889fd3c92a
root <root@a690306934.local>
2024-07-26 14:21:31 +0000

    Обновлен scheduler.py для частого запуска скриптов

--- scheduler.py
+++ scheduler.py
@@ -4,11 +4,12 @@ import subprocess
 
 def run_login_and_post_script():
     subprocess.run(["python3", "loginAndPost.py"])
-
+    print("Login and Post ended!")
+	
 def run_signup_script():
     subprocess.run(["python3", "signUp.py"])
 
-schedule.every(10).minutes.do(run_login_and_post_script)
+schedule.every(1).minutes.do(run_login_and_post_script)
 
 # Schedule the sign-up script to run twice a day (every 12 hours)
 schedule.every(12).hours.do(run_signup_script)
