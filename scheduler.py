ea5d6141041a1b96afaa87e0f618a2fdf3c3ac6a
kraievskyi <bogdan.1233211233213@gmail.com>
2024-07-26 17:18:55 +0300

    edit schedule.every(10).minutes.do(run_signup_script)

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,7 @@ def run_signup_script():
 schedule.every(10).minutes.do(run_login_and_post_script)
 
 # Schedule the sign-up script to run twice a day (every 12 hours)
-schedule.every(12).hours.do(run_signup_script)
+schedule.every(10).minutes.do(run_signup_script)
 
 while True:
     schedule.run_pending()
