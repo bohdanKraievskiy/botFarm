e4be797103d206c376bf399a476ccd9b5b06e9bd
kraievskyi <bogdan.1233211233213@gmail.com>
2024-07-26 13:03:27 +0300

    First commit

--- scheduler.py
+++ scheduler.py
@@ -2,11 +2,17 @@ import schedule
 import time
 import subprocess
 
-def run_script():
+def run_login_and_post_script():
     subprocess.run(["python", "loginAndPost.py"])
 
-# Schedule the script to run every 4 hours
-schedule.every(4).hours.do(run_script)
+def run_signup_script():
+    subprocess.run(["python", "signUp.py"])
+
+# Schedule the login and post script to run every 4 hours
+schedule.every(4).hours.do(run_login_and_post_script)
+
+# Schedule the sign-up script to run every 6 hours
+schedule.every(6).hours.do(run_signup_script)
 
 while True:
     schedule.run_pending()
